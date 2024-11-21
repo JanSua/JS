6c2368d98f324b5c2e0a46b3314b1cbf2153a1cc
JanSua <jannicolsua@hotmail.com>
2024-11-20 22:43:43 -0500

    More bootstrap development and styling

--- suppliers_app/forms.py
+++ suppliers_app/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Invoice, Supplier
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 class CreateNewInvoice(forms.ModelForm):
     class Meta:
@@ -25,13 +27,21 @@ class CreateNewInvoice(forms.ModelForm):
 
 
 class CreateNewSupplier(forms.Form):
-    companyName = forms.CharField(label="Nombre compañía",max_length=80, widget = forms.TextInput(attrs={'class':'input'}))  # Campo obligatorio
-    companyNIT = forms.CharField(label="NIT compañía",max_length=12, widget = forms.TextInput(attrs={'class':'input'}))   # Campo obligatorio
-    contactPerson = forms.CharField(label="Nombre persona de contacto",max_length=80, required=False, widget = forms.TextInput(attrs={'class':'input'}))
-    tel1 = forms.CharField(label="Teléfono de contacto 1",max_length=15, required=False, widget = forms.TextInput(attrs={'class':'input'}))
-    tel2 = forms.CharField(label="Teléfono de contacto 2",max_length=15, required=False, widget = forms.TextInput(attrs={'class':'input'}))
-    email = forms.CharField(label="Email",max_length=80, required=False, widget = forms.TextInput(attrs={'class':'input'}))
-    ciudad = forms.CharField(label="Ciudad (principal) Compañía",max_length=25, required=False, widget = forms.TextInput(attrs={'class':'input'}))
-    addrs = forms.CharField(label="Dirección Compañía",max_length=80, required=False, widget = forms.TextInput(attrs={'class':'input'}))
-    comment = forms.CharField(label="Comentarios adicionales", required=False, widget = forms.Textarea(attrs={'class':'input'}))  # TextField para comentarios 
+    companyName = forms.CharField(label="Nombre compañía",max_length=80, widget = forms.TextInput(attrs={'class':'form-control'}))  # Campo obligatorio
+    companyNIT = forms.CharField(label="NIT compañía",max_length=12, widget = forms.TextInput(attrs={'class':'form-control'}))   # Campo obligatorio
+    contactPerson = forms.CharField(label="Nombre persona de contacto",max_length=80, required=False, widget = forms.TextInput(attrs={'class':'form-control'}))
+    tel1 = forms.CharField(label="Teléfono de contacto 1",max_length=15, required=False, widget = forms.TextInput(attrs={'class':'form-control'}))
+    tel2 = forms.CharField(label="Teléfono de contacto 2",max_length=15, required=False, widget = forms.TextInput(attrs={'class':'form-control'}))
+    email = forms.CharField(label="Email",max_length=80, required=False, widget = forms.TextInput(attrs={'class':'form-control'}))
+    ciudad = forms.CharField(label="Ciudad (principal) Compañía",max_length=25, required=False, widget = forms.TextInput(attrs={'class':'form-control'}))
+    addrs = forms.CharField(label="Dirección Compañía",max_length=80, required=False, widget = forms.TextInput(attrs={'class':'form-control'}))
+    comment = forms.CharField(label="Comentarios adicionales", required=False, widget = forms.Textarea(attrs={'class':'form-control'}))  # TextField para comentarios 
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-md-3'
+        self.helper.field_class = 'col-md-9'
+        self.helper.add_input(Submit('submit', 'Guardar'))
 
--- suppliers_app/urls.py
+++ suppliers_app/urls.py
@@ -9,7 +9,13 @@ urlpatterns = [
     path('suppliers/<int:id>', views.supplier_detail, name="supplier_detail"),
     path('invoices/', views.invoices, name="invoices"),
     path('create_invoice/', views.create_invoice, name="create_invoice"),
-    path('create_supplier/', views.create_supplier, name="create_supplier"),
+    path('create_invoice/<int:id>/', views.create_invoice, name='edit_invoice'),
+    path('delete_invoice/<int:id>/', views.delete_invoice, name='delete_invoice'),
+    #path('create_supplier/', views.create_supplier, name="create_supplier"),
+    path('delete_supplier/<int:id>', views.delete_supplier, name="supplier_delete"),
+    path('supplier/new/', views.create_supplier, name='create_supplier'),
+    path('supplier/edit/<int:supplier_id>/', views.create_supplier, name='edit_supplier'),
+    
 
 
 ]
